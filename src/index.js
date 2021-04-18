05eb95a28bfe9d9021e4c0d984169ceb23526e47
Thaís Damásio <thais.damasio@outlook.com.br>
2021-04-18 12:52:24 -0300

    minor fix

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import '../assets/style/style.css';
 import Game from './Game.js';
 
-const canvas = document.querySelector("canvas")
+const canvas = document.querySelector("canvas");
 
 const GamePacheco = new Game(canvas);
 
